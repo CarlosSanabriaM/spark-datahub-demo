67571af5f82106abc55f0f62d2025274b6fe744e
Carlos Sanabria Miranda <carlos.sanabria@gruposantander.com>
2024-12-11 10:06:13 +0100

    feat: write datahub output events to file instead of send them to http endpoint

--- datahub/src/main/scala/org/example/SparkDatahubExample.scala
+++ datahub/src/main/scala/org/example/SparkDatahubExample.scala
@@ -9,6 +9,10 @@ import java.io.File
 object SparkDatahubExample extends App {
   private val peopleCsvFilePath = "people.csv"
   private val outputFolderPath = "datahub/target/developers-under-30"
+  private val datahubOutputEventsJsonFilePath = "datahub/target/datahub-output-events.jsonl"
+
+  // Delete file before running the job to avoid appending to the file
+  new File(datahubOutputEventsJsonFilePath).delete()
 
   // Create Spark session with OpenLineage configuration
   private val spark = SparkSession
@@ -20,9 +24,12 @@ object SparkDatahubExample extends App {
     .config("spark.extraListeners", "datahub.spark.DatahubSparkListener")
     // Specify the ways to emit metadata. By default, it sends to DataHub using REST emitter.
     // Valid options are rest, kafka or file
-    .config("spark.datahub.emitter", "rest")
+    .config("spark.datahub.emitter", "file")
+    // The file where metadata will be written if file emitter is set
+    .config("spark.datahub.file.filename", datahubOutputEventsJsonFilePath)
+    //.config("spark.datahub.emitter", "rest")
     // The DataHub REST server URL
-    .config("spark.datahub.rest.server", "http://localhost:8080")
+    //.config("spark.datahub.rest.server", "http://localhost:8080")
     // Include scheme from the path URI (e.g. hdfs://, s3://) in the dataset URN.
     // We recommend setting this value to false. Default is true for backwards compatibility with previous versions.
     .config("spark.datahub.metadata.include_scheme", "false")
